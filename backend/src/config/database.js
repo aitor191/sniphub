405d0f6e629a346407ecbf64d4df7fcc39fa153b
Aitor <rsaitor191@gmail.com>
2025-10-27 13:48:26 +0100

    Arreglado bug

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -6,7 +6,7 @@ const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '', // Ahora puede estar vacío
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'sniphub_db',
   waitForConnections: true,
   connectionLimit: 10,
