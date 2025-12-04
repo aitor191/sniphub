c865b8cc77e88b3c1af8c42a7054f2c7815cec78
Aitor <rsaitor191@gmail.com>
2025-12-04 12:45:56 +0100

    Creado interfaz de autenticamiento

---
+++ frontend/src/shared/interfaces/auth.interface.ts
@@ -0,0 +1,26 @@
+export interface LoginRequest {
+    email: string;
+    password: string;
+  }
+  
+  export interface RegisterRequest {
+    username: string;
+    email: string;
+    password: string;
+    full_name?: string;
+    avatar_url?: string;
+  }
+  
+  export interface AuthResponse {
+    message: string;
+    token: string;
+    user: User;
+  }
+  
+  export interface User {
+    id: number;
+    username: string;
+    email: string;
+    full_name?: string;
+    avatar_url?: string;
+  }
