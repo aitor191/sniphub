22b91219e2d76cae3ce6c1dbbb9ac0e2116720bb
Aitor <rsaitor191@gmail.com>
2025-12-09 23:10:30 +0100

    Prueba de deploy

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4000/api'
+  //apiUrl: 'http://localhost:4000/api'
+  apiUrl: 'https://sniphub-production.up.railway.app/api'
 };
